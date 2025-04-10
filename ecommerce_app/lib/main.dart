ff848d7dcc5146d9e42afca3e73584554fd2037c
programadorlcgv <programadorlcgv@gmail.com>
2025-04-10 10:05:41 -0500

    Se configuró la integración inicial con Firebase (setup del proyecto)

--- ecommerce_app/lib/main.dart
+++ ecommerce_app/lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:ecommerce_app/core/configs/theme/app_theme.dart';
 import 'package:ecommerce_app/presentation/splash/bloc/splash_cubit.dart';
 import 'package:ecommerce_app/presentation/splash/pages/splash.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(const MyApp());
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,14 +21,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: ( context) => SplashCubit()..appStarted(),
+      create: (context) => SplashCubit()..appStarted(),
       child: MaterialApp(
-        title: 'Tienda de Ropa Deportiva',
         theme: AppTheme.appTheme,
         debugShowCheckedModeBanner: false,
         home: const SplashPage()
-        
       ),
     );
+
   }
 }
