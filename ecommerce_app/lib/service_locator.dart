32765580c2ca104642b31043026f634fbf509600
programadorlcgv <programadorlcgv@gmail.com>
2025-04-17 08:25:14 -0500

    Añadido el caso de uso al Service Locator para facilitar su acceso e instanciación.

--- ecommerce_app/lib/service_locator.dart
+++ ecommerce_app/lib/service_locator.dart
@@ -3,6 +3,7 @@
 import 'package:ecommerce_app/data/auth/repository/auth_repository_imp.dart';
 import 'package:ecommerce_app/data/auth/source/auth_firebase_service.dart';
 import 'package:ecommerce_app/domain/auth/repository/auth.dart';
+import 'package:ecommerce_app/domain/auth/repository/usecases/signup.dart';
 import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
@@ -22,4 +23,9 @@ Future<void> initializeDependecies() async {
     );
 
     //usecases
+
+    sl.registerSingleton<SignupUsecase>(
+      SignupUsecase()
+    );
+
 }
